b1d59a1defca57176a6a90550ccca7413578fcf0
Tim Whitaker <tmwhtkr@gmail.com>
2018-10-04 14:55:42 -0600

    Finish rnn

--- src/model.py
+++ src/model.py
@@ -2,8 +2,23 @@ import torch
 import torch.nn as nn
 
 class RNN(nn.Module):
-	def __init__():
+	def __init__(self, input_size, hidden_size, output_size, num_layers=1):
+		super(RNN, self).__init__()
 
-	def forward():
+		self.input_size = input_size
+		self.hidden_size = hidden_size
+		self.output_size = output_size
+		self.num_layers = num_layers
 
-	def init_hidden():
+		self.encoder = nn.Embedding(self.input_size, self.hidden_size)
+		self.gru = nn.GRU(self.hidden_size, self.hidden_size, num_layers)
+		self.decoder = nn.Linear(self.hidden_size, self.output_size)
+
+	def forward(self, input, hidden):
+		input = self.encoder(input.view(1, -1))
+		output, hidden = self.gru(input.view(1, 1, -1), hidden)
+		output = self.decoder(output.view(1, -1))
+		return output, hidden
+
+	def init_hidden(self):
+		return Variable(torch.zeros(self.num_layers, 1, self.hidden_size))
